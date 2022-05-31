a3fc26c13081fac78ae427b7e2340c3bb8f7526f
Hugo Ferreira <71095540+guhzoide@users.noreply.github.com>
2022-05-31 16:55:00 -0300

    Update menu.py

--- scan/menu.py
+++ scan/menu.py
@@ -7,7 +7,7 @@ def main():
         [sg.Text('Bem-vindo(a)')],
         [[sg.Image("logo.jpg")]],
         [sg.Text('O que deseja fazer')],
-        [sg.Button('Indentificação', size=(15,0)), sg.Button('Cadastro', size=(15,0)), sg.Button('Atualizar banco local', size=(15,0)), sg.Button('Baixar tentativas', size=(15,0))]
+        [sg.Button('Indentificação', size=(15,0)), sg.Button('Cadastro', size=(15,0))]
     ]
 
     window = sg.Window('Menu', menu, element_justification='c', size=(650,350))
@@ -26,16 +26,4 @@ def main():
         window.close()
         cad = 'python3 scan/cadastro.py'
         os.system(cad)
-        os._exit(0)
-
-    elif e == 'Atualizar banco local':
-        window.close()
-        atu = 'python3 scan/atualiza_banco_local.py'
-        os.system(atu)
-        os._exit(0)
-    
-    elif e == 'Baixar tentativas':
-        window.close()
-        atu = 'python3 scan/baixar_tentativa.py'
-        os.system(atu)
         os._exit(0)
